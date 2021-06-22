3dfe87894ae24565c75b5ffb205041b8d8eec3c1
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-06-22 23:45:26 +0200

    [#1] check if issue|pr line starts with criteria

--- src/MatchTitleText.php
+++ src/MatchTitleText.php
@@ -6,11 +6,15 @@ namespace Pheature\Changelog\Filter;
 
 final class MatchTitleText
 {
+    private const LINE_TITLE_PATTERN = '`^\-\s%s.*$`';
 
-    /**
-     * MatchTitleText constructor.
-     */
-    public function __construct()
+    public function isSatisfiedBy(string $match, string $line): bool
     {
+        $regex = sprintf(self::LINE_TITLE_PATTERN, preg_quote(escapeshellcmd($match)));
+        if (preg_match($regex, $line)) {
+            return true;
+        }
+
+        return false;
     }
 }
